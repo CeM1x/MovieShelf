103ce4b2de70f879df704b6e1afd70b2654fffe8
Siamen <cemix07@mail.ru>
2025-11-09 19:40:45 +0300

    Create schemas.py

---
+++ src/schemas.py
@@ -0,0 +1,71 @@
+import datetime
+from typing import Optional, List
+
+from pydantic import BaseModel, EmailStr, Field
+
+
+# ------------------ User ------------------
+
+class UserCreateSchema(BaseModel):
+    email: EmailStr
+    password: str  # чистый пароль, хешируется на сервере
+
+class UserReadSchema(BaseModel):
+    id: int
+    email: EmailStr
+    created_at: datetime.datetime
+
+    model_config = {
+        "from_attributes": True
+    } # чтобы SQLAlchemy модели могли конвертироваться в Pydantic
+
+
+# ------------------ Review ------------------
+
+class ReviewCreateSchema(BaseModel):
+    text: Optional[str]
+    score: float
+    movie_id: int # ID фильма, к которому пишется отзыв
+
+    model_config = {
+        "from_attributes": True
+    }
+
+class ReviewReadSchema(BaseModel):
+    user_id: int
+    movie_id: int
+    score: float
+    text: Optional[str]
+
+    model_config = {
+        "from_attributes": True
+    }
+
+
+# ------------------ Movie ------------------
+
+class MovieAddSchema(BaseModel):
+    tmdb_id: int
+    title: str
+    genre: Optional[str] = None
+    description: Optional[str] = None
+    rating: Optional[float] = 0.0
+    owner_id: Optional[int] = None  # если фильм ещё не привязан к пользователю
+
+    model_config = {
+        "from_attributes": True
+    }
+
+class MovieReadSchema(BaseModel):
+    id: int
+    tmdb_id: int
+    title: str
+    genre: Optional[str] = None
+    description: Optional[str] = None
+    rating: float
+    owner_id: Optional[int] = None
+    reviews: list[ReviewReadSchema] = []  # просто пустой список
+
+    model_config = {
+        "from_attributes": True
+    }
